3959ece2164f0a95696fb71af2cd4984a1fd61dd
fischera715 <fischera@sunypoly.edu>
2026-02-20 14:22:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -63,18 +63,22 @@ st.write("For all positions except goalkeepers, there is a strong positive corre
          "suggests that players who engage in more tackles also tend to generate more interceptions, highlighting defensive effectiveness.")
 
 # Heatmap of fouls and cards by position
-foul_metrics = ["fouls_committed", "yellow_cards", "red_cards"]
-position_average = df.groupby('position')[foul_metrics].mean
+foul_metrics = ['fouls_committed', 'yellow_cards', 'red_cards']
+position_avg = df.groupby('position')[foul_metrics].mean()
 
 heatmap = px.imshow(
-  position_average, labels = dict(x = "Metric", y = "Position", color = "Average Count"), x = foul_metrics, y = position_average.index, 
-  color_continuous_scale = 'Reds', title = "Average Fouls and Cards by Position"
+    position_avg,
+    labels=dict(x="Metric", y="Position", color="Average Count"),
+    x=foul_metrics,
+    y=position_avg.index,
+    color_continuous_scale='Reds',
+    title="Average Fouls and Cards by Position"
 )
 
-st.plotly_chart(heatmap, use_container_width = True)
+st.plotly_chart(heatmap, use_container_width=True)
 
-st.write(f"This heatmap shows the average fouls, yellow cards, and red cards by position. It highlights which positions tend to commit more fouls"
-         f" or recieve more cards.")
+st.write("This heatmap shows the average fouls, yellow cards, and red cards by position. "
+         "It highlights which positions tend to commit more fouls or receive more cards.")
 
 
 
