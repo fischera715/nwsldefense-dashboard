d57f6aab4905e562b41adc0728dcfbf9a6a6c6f6
fischera715 <fischera@sunypoly.edu>
2026-02-20 13:08:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello Streamlit!")
-st.write("This is my first test app.")
+st.title("NWSL Defensive Stats")
+
+# Load CSV
+df = pd.read_csv("nwsldefense2023.csv")
+
+st.write("Here are the first 5 rows of the data:")
+st.dataframe(df.head())
