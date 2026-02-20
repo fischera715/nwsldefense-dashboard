095eb9f398c47654ada26a79d0311003d718e044
fischera715 <fischera@sunypoly.edu>
2026-02-20 13:09:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title("NWSL Defensive Stats")
 
 # Load CSV
-df = pd.read_csv("nwsldefense2023.csv")
+df = pd.read_csv("2023_nwsl_full.csv")
 
 st.write("Here are the first 5 rows of the data:")
 st.dataframe(df.head())
